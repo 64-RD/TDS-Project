2293fba1c949469fbc1ecb65ff9c23cd8c27a156
64-RD <47426142+64-RD@users.noreply.github.com>
2021-04-29 15:56:25 +0200

    Basic project upload
    Added Player with simple shooting mechanic

---
+++ Assets/Player/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Search;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+    private Vector2 _moveDirection;
+    private Vector3 _mousePos;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        _moveDirection = new Vector2(moveX, moveY).normalized;
+        _mousePos = Input.mousePosition;
+        _mousePos.z = 5.23f;
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(_moveDirection.x * moveSpeed, _moveDirection.y * moveSpeed);
+        Vector3 objectPos = Camera.main.WorldToScreenPoint (transform.position);
+        _mousePos.x -= objectPos.x;
+        _mousePos.y -= objectPos.y;
+ 
+        float angle = Mathf.Atan2(_mousePos.y, _mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle-90));
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
