f5299f0bc360b5d7183fb025fc2b111ad43ef649
64-RD <47426142+64-RD@users.noreply.github.com>
2021-05-04 12:55:15 +0200

    Upload fix
    yeah it didn't work

--- Assets/Bullet/Bullet.cs
+++ Assets/Bullet/Bullet.cs
@@ -1,11 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // yes
     public float speed = 20f;
+    public int damage = 25;
     public Rigidbody2D rb;
     
     // Start is called before the first frame update
@@ -13,5 +14,15 @@ public class Bullet : MonoBehaviour
     {
         rb.velocity = transform.up * speed;
         Destroy(gameObject, 1);
+    }
+
+    private void OnTriggerEnter2D(Collider2D hit)
+    {
+        Enemy enemy = hit.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
     }
 }
