0542bef2798db825701f39cbeb47c198c5e3af56
Daniel Zwierzchowski <danielzwierzchowski754@gmail.com>
2021-12-18 23:24:06 +0100

    fixed HealthBar after merge

--- Assets/HealthBar/HealthBar.cs
+++ Assets/HealthBar/HealthBar.cs
@@ -8,16 +8,24 @@ public class HealthBar : MonoBehaviour
     public Slider slider;
     public Gradient gradient;
     public Image fill;
+    public Text HpText;
+    public int MaxHealth;
+    public int Health;
     public void SetMaxHealth(int health)
     {
+        this.MaxHealth = health;
         slider.maxValue = health;
         slider.value = health;
+        HpText.text = $"{health}/{health}";
         fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 
     public void SetHealth(int health)
     {
+        this.Health = health;
         slider.value = health;
+        HpText.text = $"{health}/{MaxHealth}";
+
         fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
